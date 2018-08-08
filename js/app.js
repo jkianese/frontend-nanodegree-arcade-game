62d600eb7c566092b3d92afc5d40536dae82a3e5
Jim Kianese <jkianese@gmail.com>
2018-08-07 21:27:32 -0400

    Bugs moving at random speeds
    Bugs moving at random speeds

--- js/app.js
+++ js/app.js
@@ -1,55 +1,90 @@
 // Enemies our player must avoid
-const Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const Enemy = function(x, y, speed) { // Variables applied to each of our instances go here,
+    this.x = x;
+    this.y = y;
+    this.speed = 100 + Math.random() * 222; // random speed to start
+    this.sprite = 'images/enemy-bug.png'; // The image/sprite for our enemies, this uses a helper we've provided to easily load images
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+Enemy.prototype.update = function(dt) { // Update the enemy's position, required method for game Parameter: dt, a time delta between ticks
+    this.x += this.speed * dt; // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
+
+    if (this.x > 500) {
+        this.x = -50;
+        this.speed = 100 + Math.random() * 222
+    }
+    // collision with bug, player goes back to starting point
+    if (player.x < this.x + 80 && 
+        player.x + 80 > this.x &&
+        player.y < this.y + 60 &&
+        60 + player.y > this.y) {
+            player.x = 202;
+            player.y = 405;
+        }
+};        
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function() { 
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-class Player {
+class Player { 
     constructor() {
-    this.x = 0;
-    this.y = 0; 
+    this.x = 202;
+    this.y = 405; 
     this.player = 'images/char-cat-girl.png'; 
     }
-}
+// This class requires an update(), render() and
+// a handleInput() method.
+    update() {
 
-Player.prototype.update = function (dt) {
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.player), this.x, this.y);
+    }
+    handleInput(keyPress) {
+        if (keyPress == 'left' && this.x > 0) {
+            this.x -= 102;
+        }
+        if (keyPress == 'right' && this.x < 405) {
+            this.x += 102; 
+        }
+        if (keyPress == 'up' && this.y > 0) {
+            this.y -= 83; 
+        }
+        if (keyPress == 'down' && this.y < 405) {
+            this.y += 83;
+        }
+        if (this.y < 0) {
+            setTimeout(function () {
+                player.x = 202;
+                player.y = 405; 
+            }, 600);
 
-}
-
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+        }
+    }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [];
+const enemyLocation = [63, 147, 230];
 
+
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
+});
+
+const player = new Player(); 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
